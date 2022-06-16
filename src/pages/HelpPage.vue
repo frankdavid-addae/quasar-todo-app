62ac65873ec53d75e90e3aa8229e122bd8d3e50e
Frank David Addae <f.davidaddae@korba365.com>
2022-06-16 11:11:38 +0000

    complete project

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti inventore repudiandae voluptas ad soluta, eveniet consequuntur, commodi magnam, delectus qui quas aspernatur. Blanditiis dignissimos iste asperiores necessitatibus delectus eius quibusdam?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti inventore repudiandae voluptas ad soluta, eveniet consequuntur, commodi magnam, delectus qui quas aspernatur. Blanditiis dignissimos iste asperiores necessitatibus delectus eius quibusdam?</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelpPage'
+})
+</script>
